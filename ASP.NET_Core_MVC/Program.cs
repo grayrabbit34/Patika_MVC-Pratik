dd86c163e30f74ddc877a53860cebe28d0896629
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-07-21 22:12:44 +0300

    change

--- ASP.NET_Core_MVC/Program.cs
+++ ASP.NET_Core_MVC/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CustomerOrders}/{action=Index}");
 
 app.Run();
 
